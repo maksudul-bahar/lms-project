e15c85f1d59745fb7ded8e17287d32e440df6ed9
maksudul bahar <maksudul.baharkhan@gmail.com>
2026-01-21 03:47:12 +0600

    to deploy

--- lms-api/config/db.js
+++ lms-api/config/db.js
@@ -1,14 +1,31 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
+const isProduction = process.env.NODE_ENV === "production";
+
+let sequelize;
+
+if (isProduction) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+} else {
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        logging: false,
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+      logging: false,
     }
-);
+  );
+}
 
 module.exports = sequelize;
